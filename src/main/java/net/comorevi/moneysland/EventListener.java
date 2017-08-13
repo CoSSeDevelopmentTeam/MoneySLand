0f93a4569f0c11d41276862c4ebd4613e41d6ac5
popkechupki <hobbypopkechupki@gmail.com>
2017-08-13 22:23:43 +0900

    Update EventListener.java

--- src/main/java/net/comorevi/moneysland/EventListener.java
+++ src/main/java/net/comorevi/moneysland/EventListener.java
@@ -1,9 +1,15 @@
 package net.comorevi.moneysland;
 
 import net.comorevi.moneysland.MoneySLand;
+import cn.nukkit.IPlayer;
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.event.Event;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.block.BlockEvent;
+import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.block.BlockPlaceEvent;
 
 public class EventListener implements Listener {
 	
@@ -11,6 +17,28 @@ public class EventListener implements Listener {
 
 	public EventListener(MoneySLand plugin) {
 		plugin = plugin;
+	}
+	
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
+		onEvent(event);
+	}
+	
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event) {
+		onEvent(event);
+	}
+	
+	public void onEvent(Event event) {
+		Player player = ((IPlayer) event).getPlayer();
+		Block block = ((BlockEvent) event).getBlock();
+		String world = player.getLevel().getFolderName();
+		/*
+		if(plugin.isEditable(ここ)) {
+			event.setCancelled();
+			player.sendMessage("MoneySLand>>cancelled.");
+		}
+		*/
 	}
 	
 }
