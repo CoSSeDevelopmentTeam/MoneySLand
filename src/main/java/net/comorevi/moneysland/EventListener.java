e357af63f27796eea26148800fda3bd783e32c0d
itsu020402 <itsu020402@gmail.com>
2017-08-13 22:46:09 +0900

    Fix(?)

--- src/main/java/net/comorevi/moneysland/EventListener.java
+++ src/main/java/net/comorevi/moneysland/EventListener.java
@@ -1,44 +1,43 @@
 package net.comorevi.moneysland;
 
-import net.comorevi.moneysland.MoneySLand;
 import cn.nukkit.IPlayer;
 import cn.nukkit.Player;
 import cn.nukkit.block.Block;
 import cn.nukkit.event.Event;
-import cn.nukkit.event.Listener;
 import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.block.BlockEvent;
+import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.block.BlockEvent;
 import cn.nukkit.event.block.BlockPlaceEvent;
 
 public class EventListener implements Listener {
-	
-	private MoneySLand plugin;
 
-	public EventListener(MoneySLand plugin) {
-		plugin = plugin;
-	}
-	
-	@EventHandler
-	public void onBlockBreak(BlockBreakEvent event) {
-		onEvent(event);
-	}
-	
-	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent event) {
-		onEvent(event);
-	}
-	
-	public void onEvent(Event event) {
-		Player player = ((IPlayer) event).getPlayer();
-		Block block = ((BlockEvent) event).getBlock();
-		String world = player.getLevel().getFolderName();
-		/*
-		if(plugin.isEditable(ここ)) {
-			event.setCancelled();
-			player.sendMessage("MoneySLand>>cancelled.");
-		}
-		*/
-	}
-	
+    private MoneySLand plugin;
+
+    public EventListener(MoneySLand plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        onEvent(event);
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        onEvent(event);
+    }
+
+    public void onEvent(Event event) {
+        Player player = ((IPlayer) event).getPlayer();
+        Block block = ((BlockEvent) event).getBlock();
+        String world = player.getLevel().getFolderName();
+
+        if(plugin.isEditable((int)block.x, (int)block.z, world, player)) {
+            event.setCancelled();
+            player.sendMessage("MoneySLand>>cancelled.");
+        }
+
+    }
+
 }
--- src/main/java/net/comorevi/moneysland/MoneySLand.java
+++ src/main/java/net/comorevi/moneysland/MoneySLand.java
@@ -1,24 +1,52 @@
 package net.comorevi.moneysland;
 
+import cn.nukkit.Player;
 import cn.nukkit.plugin.PluginBase;
-import net.comorevi.moneysland.SQLite3DataProvider;
 
 public class MoneySLand extends PluginBase {
-	
-	private SQLite3DataProvider sql;
-	
-	@Override
-	public void onEnable() {
-		getDataFolder().mkdir();
-		getServer().getPluginManager().registerEvents(new EventListener(this), this);
-	}
-	
-	@Override
-	public void onDisable() {
-		
-	}
-	
-	public SQLite3DataProvider getSQL() {
-		return this.sql;
-	}
+
+    private SQLite3DataProvider sql;
+    private static MoneySLand instance;
+
+    @Override
+    public void onEnable() {
+        getDataFolder().mkdir();
+        getServer().getPluginManager().registerEvents(new EventListener(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public SQLite3DataProvider getSQL() {
+        return this.sql;
+    }
+
+    public static MoneySLand getInstance(){
+        return instance;
+    }
+
+    public boolean isEditable(int x, int z, String world, Player player){
+        if(player.isOp())return true;
+        int land = this.getSQL().getLand(x, z, world);
+        if(land == 0){
+            return !(this.isWorldProtect());
+        }
+
+        String name = player.getName().toLowerCase();
+
+        /* わからん()
+        if(land["owner"].equals(name)){
+            return true;
+        }
+        */
+
+        //return this.getSQL().existsGuest(land["id"], name);
+        return false;//仮。エラー回避
+    }
+
+    public boolean isWorldProtect(){
+        return false;
+    }
 }
