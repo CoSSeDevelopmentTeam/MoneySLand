345ae6793e23c14cac82780d06c9bba369c1a10a
itsu020402 <itsu020402@gmail.com>
2017-08-13 23:14:38 +0900

    Command

--- src/main/java/net/comorevi/moneysland/MoneySLand.java
+++ src/main/java/net/comorevi/moneysland/MoneySLand.java
@@ -2,12 +2,16 @@
 package net.comorevi.moneysland;
 
 import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.ConsoleCommandSender;
 import cn.nukkit.plugin.PluginBase;
 
 public class MoneySLand extends PluginBase {
 
     private SQLite3DataProvider sql;
     private static MoneySLand instance;
+    private String messages[];
 
     @Override
     public void onEnable() {
@@ -49,5 +53,56 @@ public class MoneySLand extends PluginBase {
 
     public boolean isWorldProtect(){
         return false;
+    }
+
+    public boolean onCommand(final CommandSender sender, Command command, String label, String[] args){
+
+        if(command.getName().equals("land")){
+
+            if(sender instanceof ConsoleCommandSender){
+                sender.sendMessage("MoneySLand>>注意 landコマンドはゲーム内からの実行のみ許可されています。");
+                return true;
+            }
+
+            try{if(args[0] != null){}}
+            catch(ArrayIndexOutOfBoundsException e){
+                this.helpMessage(sender);
+                return true;
+            }
+
+            String name = sender.getName().toLowerCase();
+
+            switch(args[0]){
+                case "start":
+                    return true;
+
+                case "end":
+                    return true;
+
+                case "buy":
+                    return true;
+
+                case "sell":
+                    return true;
+
+                case "invite":
+                    return true;
+
+                case "info":
+                    return true;
+
+                case "help":
+                    this.helpMessage(sender);
+                    return true;
+            }
+
+        }
+        return false;
+    }
+
+    public void helpMessage(CommandSender sender){
+        //適宜書いておいてください。
+        //getMessage()は使用しなくても大丈夫だと思ったので。
+        sender.sendMessage("MoneySLand>>ヘルプ");
     }
 }
