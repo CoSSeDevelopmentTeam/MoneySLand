56ee5d79defcf9c4a4f05603b1d426f3de44a612
popkechupki <hobbypopkechupki@gmail.com>
2017-08-13 23:09:47 +0900

    えい

--- src/main/java/net/comorevi/moneysland/MoneySLand.java
+++ src/main/java/net/comorevi/moneysland/MoneySLand.java
@@ -6,29 +6,26 @@ import cn.nukkit.plugin.PluginBase;
 
 public class MoneySLand extends PluginBase {
 
-    private SQLite3DataProvider sql;
-    private static MoneySLand instance;
-
-    @Override
-    public void onEnable() {
-        getDataFolder().mkdir();
-        getServer().getPluginManager().registerEvents(new EventListener(this), this);
-    }
-
-    @Override
-    public void onDisable() {
-
-    }
-
-    public SQLite3DataProvider getSQL() {
-        return this.sql;
-    }
-
-    public static MoneySLand getInstance(){
-        return instance;
-    }
-
-    public boolean isEditable(int x, int z, String world, Player player){
+	private static MoneySLand instance;
+	private SQLite3DataProvider sql;
+	
+	public static MoneySLand getInstance() {
+		return instance;
+	}
+	
+	public /*わからない*/ getLand(int x, int z, String world) {
+		return sql.getLand(x, z, world);
+	}
+	
+	public boolean existsLand(int x, int z, String World) {
+		return sql.existsLand(x, z, world);
+	}
+	
+	public void createLand(String owner, int[] start, int[] end, String world) {
+		sql.createLand(owner, start[0], start[1], end[0], end[1], world);
+	}
+	
+	public boolean isEditable(int x, int z, String world, Player player){
         if(player.isOp())return true;
         int land = this.getSQL().getLand(x, z, world);
         if(land == 0){
@@ -46,8 +43,49 @@ public class MoneySLand extends PluginBase {
         //return this.getSQL().existsGuest(land["id"], name);
         return false;//仮。エラー回避
     }
+	
+	public boolean isWorldProtect(String world) {
+		return false;
+	}
+	
+	@Override
+	public void onEnable() {
+		getDataFolder().mkdir();
+		getServer().getPluginManager().registerEvents(new EventListener(this), this);
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+	}
+	
+	public boolean checkOverLap(int[] start, int[] end, String world) {
+		
+	}
+	
+	public String getMessage(String str, int[] param) {
+		
+	}
+	
+	public String parseMessage(String message) {
+		
+	}
+	
+	public int calculateLandPrice(String name) {
+		
+	}
+	
+	public void helpMessage(CommandSender sender) {
+		
+	}
+	
+	public SQLite3DataProvider getSQL() {
+		return this.sql;
+	}
 
-    public boolean isWorldProtect(){
-        return false;
-    }
 }
