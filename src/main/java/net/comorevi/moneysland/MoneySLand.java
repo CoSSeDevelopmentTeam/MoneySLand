e2fbfd9c9ec98fb75e63bb0d75045fa7315fbd1c
popkechupki <hobbypopkechupki@gmail.com>
2017-08-13 22:43:33 +0900

    少し更新

--- src/main/java/net/comorevi/moneysland/MoneySLand.java
+++ src/main/java/net/comorevi/moneysland/MoneySLand.java
@@ -5,7 +5,24 @@ import net.comorevi.moneysland.SQLite3DataProvider;
 
 public class MoneySLand extends PluginBase {
 	
+	private static MoneySLand instance;
 	private SQLite3DataProvider sql;
+	
+	public static MoneySLand getInstance() {
+		return instance;
+	}
+	
+	public Land getLand(int x, int z, String world) {
+		return sql.getLand(x, z, world);
+	}
+	
+	public boolean existsLand(int x, int z, String World) {
+		return sql.existsLand(x, z, world);
+	}
+	
+	public void createLand(String owner, int[] start, int[] end, String world) {
+		sql.createLand(owner, start[0], start[1], end[0], end[1], world);
+	}
 	
 	@Override
 	public void onEnable() {
